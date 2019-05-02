f99a33b8fe2cb82d05059b066f72d30d5ddd658c
doulos76 <inojyes@gmail.com>
2019-05-02 21:12:45 +0900

    add TestGit Project

---
+++ TestGit/TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by dave76 on 02/05/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
